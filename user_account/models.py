b5a6e3719ee9a78cbc511b441d84b4992f382f6c
Nguyen Ba Tien <batiencd09@gmail.com>
2021-10-21 01:14:50 +0700

    Create member model, make migrations

--- user_account/models.py
+++ user_account/models.py
@@ -2,7 +2,15 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from django.db import models
 from address.models import Country, City, District, Ward
 from role.models import Role
-from utils.enums import Gender, CustomUserStatus, RoleName
+from utils.enums import (
+    Gender,
+    CustomUserStatus,
+    RoleName,
+    MemberLabel,
+    MemberQuarantinedStatus,
+    HealthStatus,
+    Disease,
+)
 
 # Create your models here.
 
@@ -155,6 +163,76 @@ class CustomUser(AbstractBaseUser):
         to='user_account.CustomUser',
         on_delete=models.SET_NULL,
         related_name='custom_user_x_updated_by',
+        null=True,
+        blank=True,
+    )
+
+class Member(models.Model):
+
+    custom_user = models.OneToOneField(
+        to=CustomUser,
+        on_delete=models.CASCADE,
+        related_name='member_x_custom_user',
+        primary_key=False,
+        null=False,
+    )
+
+    label = models.CharField(
+        max_length=8,
+        choices=MemberLabel.choices,
+        default=MemberLabel.F0,
+        null=False,
+    )
+
+    quarantined_place_before = models.CharField(max_length=128, null=True, blank=True)
+
+    positive_tested_before = models.BooleanField(default=False, null=False)
+
+    abroad = models.BooleanField(default=False, null=False)
+
+    quarantined_at = models.DateTimeField(auto_now_add=False, auto_now=False, null=True, blank=True)
+
+    quarantined_status = models.CharField(
+        max_length=32,
+        choices=MemberQuarantinedStatus.choices,
+        default=MemberQuarantinedStatus.QUARANTINING,
+        null=False,
+    )
+
+    last_tested = models.DateTimeField(auto_now_add=False, auto_now=False, null=True, blank=True)
+
+    health_status = models.CharField(
+        max_length=32,
+        choices=HealthStatus.choices,
+        default=HealthStatus.NORMAL,
+        null=False,
+    )
+
+    health_note = models.TextField(null=True, blank=True)
+
+    positive_test = models.BooleanField(default=False, null=False)
+
+    care_staff = models.ForeignKey(
+        to=CustomUser,
+        on_delete=models.SET_NULL,
+        related_name='member_x_care_staff',
+        null=True,
+        blank=True,
+    )
+
+    background_disease = models.CharField(
+        max_length=64,
+        choices=Disease.choices,
+        default=Disease.NONE,
+        null=False,
+    )
+
+    background_disease_note = models.TextField(null=True, blank=True)
+
+    quarantine_room = models.ForeignKey(
+        to='quarantine_ward.QuarantineRoom',
+        on_delete=models.SET_NULL,
+        related_name='member_x_quarantine_room',
         null=True,
         blank=True,
     )
