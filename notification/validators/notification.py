90c08be84a87686ab04a96b34dd9044caa03d492
Tan Chau <cttan2000@gmail.com>
2022-02-08 16:11:18 +0700

    Fix bug

--- notification/validators/notification.py
+++ notification/validators/notification.py
@@ -6,7 +6,8 @@ from utils.tools import split_input_list
 class NotificationValidator(validators.AbstractRequestValidate):
 
     def is_valid_fields(self, keys: list):
-        ignorable_fields = {'title', 'description', 'url'}
+        ignorable_fields = {'title', 'description', 'url', 'page', 'page_size',
+                            'created_at_max', 'created_at_min'}
 
         set_of_keys = set(keys) - ignorable_fields
 
