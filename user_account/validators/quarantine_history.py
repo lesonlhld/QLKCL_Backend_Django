fd3222a1f9e260c30ed343659e48268032d34527
Nguyen Ba Tien <batiencd09@gmail.com>
2022-04-10 23:19:38 +0700

    Add: API fix invalid quarantine history

--- user_account/validators/quarantine_history.py
+++ user_account/validators/quarantine_history.py
@@ -41,3 +41,10 @@ class QuarantineHistoryValidator(validators.AbstractRequestValidate):
     def extra_validate_to_filter_quarantine_history(self):
         if hasattr(self, '_user_code') and not self.is_user_code_exist():
             raise exceptions.NotFoundException({'user_code': messages.NOT_EXIST})
+
+    def extra_validate_to_fix_empty_quarantine_history(self):
+        if hasattr(self, '_user_code') and not self.is_user_code_exist():
+            raise exceptions.NotFoundException({'user_code': messages.NOT_EXIST})
+        if hasattr(self, '_user'):
+            if self._user.role.name != 'MEMBER' or not hasattr(self._user, 'member_x_custom_user'):
+                raise exceptions.ValidationException({'user_code': messages.ISNOTMEMBER})
