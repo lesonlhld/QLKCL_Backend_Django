e6e618cb4bd5b95118348810ff6805d3f1eaaa38
Nguyen Ba Tien <batiencd09@gmail.com>
2022-04-27 22:44:56 +0700

    Update: Swagger: Update Pandemic API

--- form/swagger_params.py
+++ form/swagger_params.py
@@ -1,5 +1,7 @@
 from drf_yasg import openapi
 
+from utils.enums import TestType
+
 # Pandemic
 
 get_pandemic_swagger_params = [
@@ -13,4 +15,18 @@ update_pandemic_swagger_params = [
     openapi.Parameter('quarantine_time_vac', in_=openapi.IN_FORM, type=openapi.TYPE_NUMBER),
     openapi.Parameter('remain_qt_cc_pos_vac', in_=openapi.IN_FORM, type=openapi.TYPE_NUMBER),
     openapi.Parameter('remain_qt_cc_pos_not_vac', in_=openapi.IN_FORM, type=openapi.TYPE_NUMBER),
+    openapi.Parameter('remain_qt_cc_not_pos_vac', in_=openapi.IN_FORM, type=openapi.TYPE_NUMBER),
+    openapi.Parameter('remain_qt_cc_not_pos_not_vac', in_=openapi.IN_FORM, type=openapi.TYPE_NUMBER),
+    openapi.Parameter('remain_qt_pos_vac', in_=openapi.IN_FORM, type=openapi.TYPE_NUMBER),
+    openapi.Parameter('remain_qt_pos_not_vac', in_=openapi.IN_FORM, type=openapi.TYPE_NUMBER),
+    openapi.Parameter('test_type_pos_to_neg_vac', in_=openapi.IN_FORM, type=openapi.TYPE_STRING, enum=list(TestType.__members__.values())),
+    openapi.Parameter('num_test_pos_to_neg_vac', in_=openapi.IN_FORM, type=openapi.TYPE_NUMBER),
+    openapi.Parameter('test_type_pos_to_neg_not_vac', in_=openapi.IN_FORM, type=openapi.TYPE_STRING, enum=list(TestType.__members__.values())),
+    openapi.Parameter('num_test_pos_to_neg_not_vac', in_=openapi.IN_FORM, type=openapi.TYPE_NUMBER),
+    openapi.Parameter('test_type_none_to_neg_vac', in_=openapi.IN_FORM, type=openapi.TYPE_STRING, enum=list(TestType.__members__.values())),
+    openapi.Parameter('num_test_none_to_neg_vac', in_=openapi.IN_FORM, type=openapi.TYPE_NUMBER),
+    openapi.Parameter('test_type_none_to_neg_not_vac', in_=openapi.IN_FORM, type=openapi.TYPE_STRING, enum=list(TestType.__members__.values())),
+    openapi.Parameter('num_test_none_to_neg_not_vac', in_=openapi.IN_FORM, type=openapi.TYPE_NUMBER),
+    openapi.Parameter('num_day_to_close_room', in_=openapi.IN_FORM, type=openapi.TYPE_NUMBER),
+    openapi.Parameter('day_between_tests', in_=openapi.IN_FORM, type=openapi.TYPE_NUMBER),
 ]
