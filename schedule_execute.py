95fc1d5bc12d15e2bd0cb2df1b5cd4d92cd75d21
Nguyen Ba Tien <batiencd09@gmail.com>
2022-05-02 01:14:17 +0700

    Add: send notification everyday to member need test

---
+++ schedule_execute.py
@@ -0,0 +1,15 @@
+#========================================
+# Scheduler Jobs
+#========================================
+import pytz
+from apscheduler.schedulers.background import BackgroundScheduler
+from user_account.views import send_notification_is_last_test
+
+scheduler = BackgroundScheduler()
+
+vntz = pytz.timezone('Asia/Saigon')
+
+scheduler.configure(timezone=vntz)
+
+scheduler.add_job(send_notification_is_last_test, 'cron', hour=1, minute=30)
+scheduler.start()
