cb8198c41f8347ca924b608ccc6459b1d51e2226
Nguyen Ba Tien <batiencd09@gmail.com>
2022-05-02 03:30:56 +0700

    Update: change time to send noti everyday

--- clock.py
+++ clock.py
@@ -18,5 +18,5 @@ vntz = pytz.timezone('Asia/Saigon')
 
 scheduler.configure(timezone=vntz)
 
-scheduler.add_job(send_notification_is_last_test, 'cron', hour=3, minute=30)
+scheduler.add_job(send_notification_is_last_test, 'cron', hour=1, minute=30)
 scheduler.start()
